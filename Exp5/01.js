4cd8a0309f386961e29b7ddebc9aaff73783a707
Rachit Garg <rachitgarg1208@gmail.com>
2024-11-03 19:50:18 +0530

    Student Management System

--- Exp5/01.js
+++ Exp5/01.js
@@ -28,14 +28,6 @@ function addStudent() {
     document.getElementById("studentScore").value = "";
 }
 
-function calculateGrade(score) {
-    if (score >= 90) return "A";
-    else if (score >= 80) return "B";
-    else if (score >= 70) return "C";
-    else if (score >= 60) return "D";
-    else return "F";
-}
-
 function displayStudents() {
     const studentList = document.getElementById("studentList");
     studentList.innerHTML = "";
